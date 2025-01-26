f34c5e122d7e279536fa9fb234b48acd797562dd
Alex Wilmer <djazium@gmail.com>
2025-01-26 07:59:32 -0700

    .,

--- api/network.tf
+++ api/network.tf
@@ -38,23 +38,23 @@ resource "aws_lb" "main" {
   subnets            = aws_subnet.public.*.id
 }
 
-resource "aws_lb_target_group" "main" {
-  name        = "pr-app-tg"
-  port        = 3000
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.main.id
-  target_type = "ip"  # Change to "ip" for Fargate
-}
-
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = aws_lb_target_group.main.arn
   }
+}
+
+resource "aws_lb_target_group" "main" {
+  name        = "pr-app-tg-unique"  # Change name to force recreation
+  port        = 3000
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"  # Crucial for Fargate
 }
 
 resource "aws_internet_gateway" "main" {
