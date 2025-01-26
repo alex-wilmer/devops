4e74c60f8b9dc45eeb526219bea044e37fdb4353
Alex Wilmer <djazium@gmail.com>
2025-01-26 07:35:34 -0700

    .

--- api/ecs.tf
+++ api/ecs.tf
@@ -1,5 +1,27 @@
 variable "docker_image" {}
 
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "pr-ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.ecs_task_execution.name
+}
+
 resource "aws_ecs_cluster" "main" {
   name = "pr-cluster"
 }
